e2a99e03c852c467f83094338d301c056906a410
BenedictaUche <benedictauche.onyebuchi@gmail.com>
2023-02-25 18:00:16 +0100

    add: added a margin-bottom to the vine

--- src/components/About.js
+++ src/components/About.js
@@ -31,7 +31,7 @@ const About = () => {
           </div>
         </div>
       </div>
-      <img src="./images/vine.png" alt="a vine" className='mx-auto block mb-4' />
+      <img src="./images/vine.png" alt="a vine" className='mx-auto block mb-[8%]' />
     </>
   );
 };
